8852d84ab74d2aca830d9c241c5e0e04f0c689dc
saccouu <yskwskk@yahoo.co.jp>
2026-07-10 14:34:06 +0900

    Update adBanners.ts

--- src/data/adBanners.ts
+++ src/data/adBanners.ts
@@ -14,12 +14,14 @@ export const AD_BANNERS = [
     htmlCode: `<a href="https://px.a8.net/svt/ejp?a8mat=3Z4WIB+45GOHE+2PEO+HUSFL&a8ejpredirect=https%3A%2F%2Fcoconala.com%2Fcategories%2F3%3Fservice_kind%3D1" rel="nofollow">
 <img border="0" alt="" src="https://www.bbauranai.jp/ads/banner1.png"></a>
 <img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=3Z4WIB+45GOHE+2PEO+HUSFL" alt="">`,
-    label: 'あなたにおすすめの場所はここよ',
+    label: 'あなたにはここもおすすめよ',
   },
   // 2つ目以降を追加する場合は、それぞれ違うidを付けてください（例）
   // {
   //   id: 'brillante',
-  //   htmlCode: `A8.netからコピーしたコードをそのまま貼り付け`,
-  //   label: 'こちらもおすすめ',
+  //   htmlCode: `<a href="https://px.a8.net/svt/ejp?a8mat=3Z4WIB+45GOHE+2PEO+HUSFL&a8ejpredirect=https%3A%2F%2Fcoconala.com%2Fcategories%2F3%3Fservice_kind%3D1" rel="nofollow">
+<img border="0" alt="" src="https://www.bbauranai.jp/ads/banner_2.png"></a>
+<img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3Z4WIB+45GOHE+2PEO+HUSFL" alt="">`,
+  //   label: 'あなたにおすすめの場所はここよ',
   // },
 ];
